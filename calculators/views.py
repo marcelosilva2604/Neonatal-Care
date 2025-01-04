653cc0c3782c66bca7d9381397e052a1a63eb119
marcelosilva2604 <marcelo_carvalhosilva@hotmail.com>
2025-01-04 18:43:17 -0300

    Antes de realizar a plotagem gráfica de bilirrubina

--- calculators/views.py
+++ calculators/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     """Renderiza a página inicial."""
     return render(request, 'calculators/index.html')
+
+def bilirubin_calculator(request):
+    return render(request, 'calculators/bilirubin.html')
--- neonatal_calculator_care/urls.py
+++ neonatal_calculator_care/urls.py
@@ -7,4 +7,5 @@ from calculators import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('bilirubin-calculator/', views.bilirubin_calculator, name='bilirubin_calculator'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
